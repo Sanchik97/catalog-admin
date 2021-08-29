ed430498d9fe2980c35f0b5826b9e1c8c5ecd8fb
Sanchik97 <sesancho97@gmail.com>
2021-08-29 18:48:38 +0500

    query token

--- src/api/makeRequest.ts
+++ src/api/makeRequest.ts
@@ -1,38 +1,39 @@
 import axios from 'axios'
-import {AUTH_TOKEN} from "@app/constants/auth"
-
+import { AUTH_TOKEN } from '@app/constants/auth'
 
 const makeRequest = axios.create({
-	baseURL: process.env.REACT_APP_API_URL
+  baseURL: process.env.REACT_APP_API_URL,
 })
 
 makeRequest.interceptors.request.use(
-	async (config) => {
+  async (config) => {
+    config.headers = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    }
 
-		config.headers = {
-			Accept: 'application/json',
-			Authorization: 'Bearer ' + localStorage.getItem(AUTH_TOKEN),
-			'Content-Type': 'application/json',
-		}
+    config.params = {
+      token: localStorage.getItem(AUTH_TOKEN),
+    }
 
-		return config
-	},
-	function (error) {
-		return Promise.reject(error)
-	},
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  },
 )
 
 makeRequest.interceptors.response.use(
-	async (response) => {
-		return response
-	},
-	(error) => {
-		if (error.response.status === 401 && window.location.pathname !== '/auth') {
-			localStorage.removeItem(AUTH_TOKEN)
-			window.location.href = '/auth'
-		}
-		return Promise.reject(error)
-	},
+  async (response) => {
+    return response
+  },
+  (error) => {
+    if (error.response.status === 401 && window.location.pathname !== '/auth') {
+      localStorage.removeItem(AUTH_TOKEN)
+      window.location.href = '/auth'
+    }
+    return Promise.reject(error)
+  },
 )
 
 export default makeRequest
